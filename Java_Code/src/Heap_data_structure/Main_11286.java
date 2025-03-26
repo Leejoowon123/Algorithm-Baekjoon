7111f1e6a7816dda34f1250b3433667c25f702a1
Leejoowon123 <joowon582@gmail.com>
2025-03-26 21:11:32 +0900

    feat: 문제 풀기

---
+++ Java_Code/src/Heap_data_structure/Main_11286.java
@@ -0,0 +1,41 @@
+package Heap_data_structure;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Comparator;
+
+public class Main_11286 {
+    
+    public void solution()throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pqueue = new PriorityQueue<>(new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1, Integer o2){
+                if(Math.abs(o1) > Math.abs(o2)){
+                    return Math.abs(o1) - Math.abs(o2);
+                }else if(Math.abs(o1) == Math.abs(o2)){
+                    return o1 - o2;
+                }else{
+                    return -1;
+                }
+            }
+        });
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            int x = Integer.parseInt(br.readLine());
+            if(x == 0){
+                if(pqueue.isEmpty()) sb.append(0).append("\n");
+                else sb.append(pqueue.poll()).append('\n');
+            }else pqueue.offer(x);
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void main(String args[])throws Exception{
+        new Main_11286().solution();
+    }
+}
---
+++ Java_Code/src/Heap_data_structure/Main_1927.java
@@ -0,0 +1,34 @@
+package Heap_data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main_1927 {
+    
+    public void solution() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for(int i = 0; i < N; i++){
+            int x = Integer.parseInt(br.readLine());
+
+            if(x > 0){
+                queue.offer(x);
+            }else{
+                if(!queue.isEmpty()) System.out.println(queue.poll()); 
+                else System.out.println(0);
+            }
+        }
+
+        br.close();
+        
+    }
+    
+    public static void main(String args [])throws Exception{
+        new Main_1927().solution();
+    }
+}
